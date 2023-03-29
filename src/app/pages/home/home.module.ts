66a7b9ce564685f3225a81f151a665410ea54d0e
denchik <v525oa@gmail.com>
2023-03-30 00:18:37 +0400

    Add callback form component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -11,6 +11,7 @@ import { ReviewsComponent } from 'src/app/pages/home/components/reviews/reviews.
 import { SpecialOfferComponent } from 'src/app/pages/home/components/special-offer/special-offer.component';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -26,14 +27,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
     ReviewsComponent,
     CallbackFormComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([{
-      path: '',
-      component: HomeComponent,
-    }]),
-    SharedModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([{
+            path: '',
+            component: HomeComponent,
+        }]),
+        SharedModule,
+        ReactiveFormsModule,
+    ],
   providers: [],
   exports: [],
 })
