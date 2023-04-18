e2acbeecc88bdb84617f5cf8168246710cb1ba5b
denchik <v525oa@gmail.com>
2023-04-19 00:48:46 +0400

    Add social block

---
+++ src/app/shared/components/social-block/social-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-block',
+  templateUrl: './social-block.component.html',
+  styleUrls: ['./social-block.component.scss']
+})
+export class SocialBlockComponent {
+
+}
