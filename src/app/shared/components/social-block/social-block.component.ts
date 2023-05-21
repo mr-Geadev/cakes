199c3bc101b7ec00ddb3d4e54195a4ac807a8433
denchik <v525oa@gmail.com>
2023-05-21 17:40:51 +0400

    Review changes

--- src/app/shared/components/social-block/social-block.component.ts
+++ src/app/shared/components/social-block/social-block.component.ts
@@ -1,4 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+type SocialLink = {
+  name: string,
+  link: string,
+  icon: string
+}
+
+export const socialLinkList : SocialLink[] = [
+  {
+    name: 'vk',
+    link: 'https://vk.com/',
+    icon: 'assets/images/icons/vk-circle.svg'
+  },
+  {
+    name: 'telegram',
+    link: 'https://t.me/Drunk_Bear19',
+    icon: 'assets/images/icons/telegram-circle.svg'
+  },
+  {
+    name: 'whatsapp',
+    link: 'https://wa.me/79000000000',
+    icon: 'assets/images/icons/whatsapp-circle.svg'
+  },
+  {
+    name: 'viber',
+    link: 'viber://chat?number=%2B79372545419',
+    icon: 'assets/images/icons/viber-circle.svg'
+  }
+]
 
 @Component({
   selector: 'app-social-block',
@@ -6,5 +35,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./social-block.component.scss']
 })
 export class SocialBlockComponent {
-
+  socialLinkList = socialLinkList;
 }
